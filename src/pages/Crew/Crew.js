63e7d8a199c16083c1bb5076cf780cf98831abd0
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-02-27 12:32:38 +0100

    Implémentation du responsive pour la page 'Crew'

--- src/pages/Crew/Crew.js
+++ src/pages/Crew/Crew.js
@@ -27,29 +27,34 @@ export default function Crew() {
       <div className="crewGlobalContainer">
 
         <div className="crewContainer">
-        <div className="titleContainer">
-          <p className='number'>02</p>
-          <h1 className='title'>MEET YOUR CREW</h1>
-        </div>
-          <div className="crewContent">
+          <div className="titleContainer">
+            <p className='number'>02</p>
+            <h1 className='title'>MEET YOUR CREW</h1>
+          </div>
+          <div className="crewGlobalContent">
+            <div className="crewContent">
 
-            <div className="crewInformations">
-              <h2 className='crewRole'>{data.role}</h2>
-              <h3 className='crewMember'>{data.name}</h3>
-              <p className="crewDescription">{data.bio}</p>
+              <div className="crewInformations">
+                <h2 className='crewRole'>{data.role}</h2>
+                <h3 className='crewMember'>{data.name}</h3>
+                <p className="crewDescription">{data.bio}</p>
+              </div>
+
+              <div className='radioContainer'>
+                {projectsData.map((member,index) => (
+                  <input key={index} type="radio" id={index} onClick={handleRadio} className={active === index ? "radioButtonActive" : "radioButton"}/>
+                  ))}
+              </div>           
+
             </div>
+ 
 
-            <div className='radioContainer'>
-              {projectsData.map((member,index) => (
-                <input key={index} type="radio" id={index} onClick={handleRadio} className={active === index ? "radioButtonActive" : "radioButton"}/>
-                ))}
-            </div>           
-
+            <div className="crewPicture">
+              <img src={data.images.webp} alt={data.name} />
+              <div className="pictureLine"></div>
+            </div>
           </div>
         </div>
-        <div className="crewPicture">
-            <img src={data.images.webp} alt={data.name} />
-      </div>
         
       </div>
 
